d6ef9d9d34d808f16e5fff767df6813bb3e83c4d
mendes-low <uskembaev07@icloud.com>
2023-12-12 15:40:43 +0600

    hw-1

---
+++ JavaScript-hw/variables-11.12.23/index.js
@@ -0,0 +1,52 @@
+// 1
+// let my_name = 'Азиз'
+// let my_last_name = 'Ускембаев'
+// let my_age = 16
+// const id = 10101010010
+// const im_bored = 'Қазақстан' 
+// let city = 'Алматы'
+// let fact = 'кітап'
+// let fact2 = 'HTML-CSS'
+// let fact3 = 'JavaScript'
+// let favorite_Color = 'қара'
+// let favorite_Song = 'believer'
+// let favorite_Movie = 'тор'
+// let favorite_Animal = 'арыстан'
+// let favorite_language = 'ағылшын'
+// let favorite_programiz_language = 'JavaScript'
+
+// let What_I_love = `  Менің атым ${my_name} ${my_last_name}. Менің жасым ${my_age}да. Менде өзімнің бірегей ЖСН-ным бар. Ол осындай:${id}. Мен ${im_bored}-да ${city} қаласында туылдым.
+//   Мен туралы бірнеше факт, мен ${fact} оқуды жақсы көремін, мен ${fact2}-ты жаксы білемін және де мен қәзір ${fact3} программалау тілін үйреніп жатырмын.
+//   Мен сүйікті заттарым. Мысалы, менің сүйікті түсім ${favorite_Color}, сүйікті әнім ${favorite_Song}, сүйікті фильмім ${favorite_Movie}, сүйікті жануарым ${favorite_Animal}, сүйікті тілім ${favorite_language}, сүікті программалу тілі ${favorite_programiz_language}`
+// console.log(What_I_love);
+
+// 2
+// let has_Coffee = true
+// if (has_Coffee == true ) {
+//   console.log('I have coffee');  
+// } else {
+//     console.log('i don\'t have coffee');
+// }
+
+// 3
+// let Weekend = false
+// if (Weekend == false) {
+//     console.log('Today is weekend');
+// } else {
+//     console.log('today is\'t weekend');
+// }
+
+// 4
+// let gravity = 9.8 
+// let result = 60 * gravity
+// console.log(result); 
+
+// 5
+// let story = -10 
+// let f_temp = story * 1.8 + 32
+// console.log(f_temp);
+
+
+
+
+

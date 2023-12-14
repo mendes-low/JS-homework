ad6999fe105754490a6cecdc8df777a887d4081e
mendes-low <uskembaev07@icloud.com>
2023-12-14 15:05:12 +0600

    hw2

---
+++ JavaScript-hw/data_types-12.12.23/index.js
@@ -0,0 +1,23 @@
+// 1
+// console.log(Math.min(14, 32, 9, 27, 41));
+
+// 2
+// console.log(`1 number:${Math.ceil(Math.min(14, 32, 9, 27, 41) / 745)}`);
+
+// 3
+// console.log(`2 number:${Math.ceil(Math.random() * 50)}`);
+
+// 4
+// console.log(`Sum 2 numbers:${Math.ceil(Math.min(14, 32, 9, 27, 41) / 745) + Math.ceil(Math.random() * 50)}`);
+
+// Level 2
+
+// 7
+// console.log(Math.ceil(Math.random() * 100));
+
+// 8
+// console.log(Math.floor(Math.random() * 101) + 50);
+
+// 9
+// console.log(Math.ceil(Math.random() * 255 ));
+

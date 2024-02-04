3a44aec57c6a1dbf00206e89d5bd04b820e5504c
mendes-low <uskembaev07@icloud.com>
2024-02-05 00:41:18 +0600

    object

---
+++ JavaScript-hw/object-22.01.24/index.js
@@ -0,0 +1,178 @@
+// Level 1
+
+// 1
+// let dog = {}
+
+// 2
+// console.log(dog);
+
+// 3
+// dog = {
+//     dogName: 'Rex',
+//     legs: 4,
+//     color: 'black',
+//     age: 7,
+//     bark: 'woof woof'
+// }
+
+// 4
+// console.log(dog.dogName);
+// console.log(dog.legs);
+// console.log(dog.color);
+// console.log(dog.age);
+// console.log(dog.bark);
+
+// 5
+// dog = {
+//     breed: 'Сhihuahua', 
+//     getDogInfo: 
+// }
+
+
+// Level 2
+
+// 1
+// const users = {
+//     Alex: {
+//         email: 'alex@alex.com',
+//         skills: ['HTML', 'CSS', 'JavaScript'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 30
+//     },
+//     Asab: {
+//         email: 'asab@asab.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//         age: 25,
+//         isLoggedIn: false,
+//         points: 50
+//     },
+//     Brook: {
+//         email: 'daniel@daniel.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//         age: 30,
+//         isLoggedIn: true,
+//         points: 50
+//     },
+//     Daniel: {
+//         email: 'daniel@alex.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 40
+//     },
+//     John: {
+//         email: 'john@john.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//         age: 20,
+//         isLoggedIn: true,
+//         points: 50
+//     },
+//     Thomas: {
+//         email: 'thomas@thomas.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 40
+//     },
+//     Paul: {
+//         email: 'paul@paul.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 40
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const user1 = {
+//     username: 'Aziz',
+//     age: 16,
+//     password: 'qwerty123',
+//     email: 'aziz2424@gmail.com',
+//     phoneNumber: 87018339372,
+//     cart: [],
+//     purchases: [],
+//     leaveReview: [],
+//     purchase: function(product) {
+//         this.purchases.push(product)
+//     },
+//     adToCart: function(product) {
+//         this.cart.push(product)
+//     },
+//     leaveReview: function(product, text) {
+//         product.review.push({author: this.username, review: text})
+//     }
+// }
+
+// const user2 = {
+//     username: 'Adil',
+//     age: 25,
+//     password: 'qw12345d',
+//     email: 'adil2020@gmail.com',
+//     phoneNumber: 87778532675,
+//     cart: [],
+//     purchases: [],
+//     purchase: function(product) {
+//         this.purchases.push(product)
+//     },
+//     adToCart: function(product) {
+//         this.cart.push(product)
+//     },
+//     leaveReview: function(product, text) {
+//         product.review.push(text)
+//     }
+// }
+
+// const product1 = {
+//     name: "MacBook",
+//     price: 150000,
+//     brand: 'Apple',
+//     description: 'screensize: 15,6',
+//     review: []
+// }
+
+// const product2 = {
+//     name: "Samsung Galaxy A51",
+//     price: 30000,
+//     brand: 'Samsung',
+//     description: 'memory: 128GB',
+//     review: []
+// }
+// const product3 = {
+//     name: "Laptop Vivobook Pro 15",
+//     price: 290000,
+//     brand: 'Acer',
+//     description: 'screensize: 15',
+//     review: []
+// }
+// const product4 = {
+//     name: "Xiaomi note 10",
+//     price: 55000,
+//     brand: 'Xiaomi',
+//     description: 'memory 512GB',
+//     review: []
+// }
+// const product5 = {
+//     name: "Iphone 10",
+//     price: 150000,
+//     brand: 'Apple',
+//     description: 'memory 256GB',
+//     review: []
+// }
+
